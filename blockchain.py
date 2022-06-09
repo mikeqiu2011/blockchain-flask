4c8dc77a0f67b2a6c2d429fb796dd2448103fb9b
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-09 13:55:08 +0800

    create block function doen

--- blockchain.py
+++ blockchain.py
@@ -3,3 +3,20 @@ import hashlib
 import json
 from flask import Flask, jsonify
 
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof=1, previous_hash='0')  # create genisis block
+
+    def create_block(self, proof, previous_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'prev_hash': previous_hash
+        }
+
+        self.chain.append(block)
+        return block
+
