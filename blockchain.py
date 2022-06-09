cec9b55b4c6984b01979cedf021a961a1122a638
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-09 14:53:08 +0800

    proof_of_work done

--- blockchain.py
+++ blockchain.py
@@ -22,3 +22,16 @@ class Blockchain:
 
     def get_last_block(self):
         return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+
+        while True:
+            payload = str(new_proof**2 - previous_proof**2).encode()
+            hash_value = hashlib.sha256(payload).hexdigest()
+            print(f'hash value is:', hash_value)
+
+            if hash_value[:4] == '0000':
+                return new_proof
+            else:
+                new_proof +=1
